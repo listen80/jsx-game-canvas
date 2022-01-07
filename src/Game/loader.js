4f016de58a02d197e3cd00c1820a3ae737bc3102
lijiabin <13631624455@163.com>
2022-01-07 09:19:34 +0800

    update

--- src/Game/loader.js
+++ src/Game/loader.js
@@ -51,26 +51,28 @@ const loadImages = () => {
 
 export const loaderImage = () => Promise.all([loadImages()])
 const sounds = [
-  'bgm/terror.mp3',
-  'bgm/prologue.mp3',
   'bgm/area1.mp3',
   'bgm/area2.mp3',
   'bgm/area3.mp3',
-  'enemy/redWizard.mp3',
-  'enemy/brownWizard.mp3',
-  'enemy/whiteKing.mp3',
+  'bgm/prologue.mp3',
+  'bgm/terror.mp3',
+
   'enemy/blackMagician.mp3',
+  'enemy/brownWizard.mp3',
+  'enemy/redWizard.mp3',
+  'enemy/whiteKing.mp3',
+
   'se/attack.mp3',
-  'se/floor.mp3',
-  'se/door.mp3',
   'se/buy.mp3',
-  'se/item.mp3',
-  'se/dialogue.mp3',
-  'se/unlockCtrl.mp3',
-  'se/step.mp3',
   'se/constants.mp3',
-  'se/sell.mp3',
+  'se/dialogue.mp3',
+  'se/door.mp3',
+  'se/floor.mp3',
+  'se/item.mp3',
   'se/load.mp3',
+  'se/relieve.mp3',
+  'se/sell.mp3',
+  'se/step.mp3',
 ]
 
 export const loaderMusic = () => Promise.all([
