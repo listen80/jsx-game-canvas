ce39c7a1d0ad9ff3cb077c1313731797271eae74
Your Name <136@163.com>
2022-09-27 07:48:15 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -7,34 +7,9 @@ import { terser } from "rollup-plugin-terser"; // 压缩代码
 import serve from "rollup-plugin-serve"; // 启动服务
 
 import replace from "rollup-plugin-replace"; // 注入环境变量
-// import html2 from "rollup-plugin-html2";
 
-// import postcss from 'rollup-plugin-postcss';
-// import vue from 'rollup-plugin-vue' // 处理vue的插件ƒ
-// import livereload from 'rollup-plugin-livereload' // 实时刷新
+import livereload from 'rollup-plugin-livereload' // 实时刷新
 
-
-const p = new Proxy(
-  {
-    tag: "script",
-    src: "bundle.js",
-  },
-  {
-    get(target, prop) {
-      const date = new Date();
-      if (prop === "src") {
-        return (
-          target[prop] +
-          `?${date.toLocaleDateString()}@${date.toTimeString().substring(0, 8)}`
-        );
-      }
-      return target[prop];
-    },
-    set(obj, prop, value) {
-      obj[prop] = value;
-    },
-  }
-);
 
 const plugins = [
   alias({
@@ -52,17 +27,7 @@ const plugins = [
   babel({
     babelHelpers: "bundled", // 多次使用辅助函数只保留一个  比如 class 在转换成es5时会使用多个辅助函数则只保留一个
   }),
-  // livereload()
-  // jsx({
-  //   factory: 'h',
-  // }),
-  // postcss(),
-  // vue(),
-  // html2({
-  //   template: "src/index.html",
-  //   inject: false,
-  //   externals: { after: [p] },
-  // }),
+  livereload()
 ];
 
 if (process.env.NODE_ENV === "development") {
@@ -84,15 +49,9 @@ export default {
     // Mota: "src/Mota",
     game: "src/",
   },
-  // input:  "src/Mota/index.jsx",
-  // Engine: "src/Engine/index.js",
   output: {
     // file: 'public/bundle.js',
     // format: "umd",
     dir: "public/Bundle",
   },
-  // output: {
-  //   Mota: "dasd.js",
-  //   Engine: "issdsndex.js",
-  // },
 };
