850c5669cb39f36acaf053b2c318a2d0e4970638
Your Name <you@example.com>
2023-01-16 19:10:42 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,48 @@
-import commonjs from '@rollup/plugin-commonjs' // 使用commonjs
-import resolve from '@rollup/plugin-node-resolve' // 加载第三方库
-import babel from '@rollup/plugin-babel' // 转成es5
-import alias from '@rollup/plugin-alias'
+import commonjs from "@rollup/plugin-commonjs"; // 使用commonjs
+import resolve from "@rollup/plugin-node-resolve"; // 加载第三方库
+import babel from "@rollup/plugin-babel"; // 转成es5
+import alias from "@rollup/plugin-alias";
 
-import { terser } from 'rollup-plugin-terser' // 压缩代码
-import serve from 'rollup-plugin-serve' // 启动服务
+import { terser } from "rollup-plugin-terser"; // 压缩代码
+import serve from "rollup-plugin-serve"; // 启动服务
 
-import replace from 'rollup-plugin-replace' // 注入环境变量
-import dotenv from 'dotenv'
+import replace from "rollup-plugin-replace"; // 注入环境变量
 
-// console.log(dotenv.config())
-// dotenv()
-const __DEV__ = process.env.NODE_ENV === 'development'
- 
+const __DEV__ = process.env.NODE_ENV === "development";
+
 const plugins = [
   alias({
     entries: {
-      Engine: require('path').resolve('src/Engine'),
-      Mota: require('path').resolve('src/Mota'),
+      Engine: require("path").resolve("src/Engine"),
+      Mota: require("path").resolve("src/Mota"),
     },
   }),
   commonjs(),
-  resolve({
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  }),
-  replace({
-    __DEV__,
-    // 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-  }),
+  resolve({ extensions: [".ts", ".tsx", ".js", ".jsx"] }),
+  replace({ __DEV__ }),
   babel({
-    babelHelpers: 'bundled', // 多次使用辅助函数只保留一个  比如 class 在转换成es5时会使用多个辅助函数则只保留一个
+    babelHelpers: "bundled", // 多次使用辅助函数只保留一个  比如 class 在转换成es5时会使用多个辅助函数则只保留一个
   }),
-]
+];
 
 if (__DEV__) {
   plugins.push(
     serve({
       // open: true,
       port: 8010,
-      contentBase: 'public',
-    }),
-  )
+      contentBase: "public",
+    })
+  );
 } else {
-  plugins.push(terser())
+  plugins.push(terser());
 }
 
 export default {
   plugins,
   input: {
-    game: 'src/',
+    game: "src/",
   },
   output: {
-    dir: 'public/Bundle',
+    dir: "public/Bundle",
   },
-}
+};
