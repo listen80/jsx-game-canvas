04f58435b0fe46cfc02382fdec9c1a5a8e3d1bd5
jiabin.li <jiabin.li@shopee.com>
2023-01-31 17:46:52 +0800

    update

--- src/Mota/game/components/Column.jsx
+++ src/Mota/game/components/Column.jsx
@@ -1,10 +1,10 @@
 export default {
   render() {
-    let x = 0;
+    let y = 0;
     return this.$chilren.map((child) => {
-      const { width = 1 } = child.attrs?.size || {};
+      const { height = 1 } = child.attrs?.size || {};
       const rowEle = <div position={{ x }}>{child}</div>;
-      x += width;
+      y += height;
       return rowEle;
     });
   },
