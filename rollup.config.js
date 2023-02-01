a01da12490cb4a1ebe2e2eaed604be457d10d6a4
jiabin.li <jiabin.li@shopee.com>
2023-02-01 17:42:13 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv"
+
 import commonjs from "@rollup/plugin-commonjs"; // 使用commonjs
 import resolve from "@rollup/plugin-node-resolve"; // 加载第三方库
 import babel from "@rollup/plugin-babel"; // 转成es5
@@ -7,6 +9,8 @@ import { terser } from "rollup-plugin-terser"; // 压缩代码
 import serve from "rollup-plugin-serve"; // 启动服务
 
 import replace from "rollup-plugin-replace"; // 注入环境变量
+
+dotenv.config()
 
 const __DEV__ = process.env.NODE_ENV === "development";
 
--- server.js
+++
@@ -1,5 +0,0 @@
-const hs = require('http-server')
-
-hs.createServer({ root: 'public' }).listen(8010, () => {
-  console.log('ok')
-})
