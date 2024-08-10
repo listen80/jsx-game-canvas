342684505132a2a1e052efc23f1f363033e291f3
开源小码农 <m13631624455@163.com>
2024-08-10 23:37:14 +0800

    update

--- src/Engine/core/Loader.js
+++ src/Engine/core/Loader.js
@@ -1,136 +1,137 @@
-import { loadJSON, loadText, loadImage } from '../utils/http'
+import { loadJSON, loadText, loadImage } from "../utils/http";
 
-const emptyImage = new Image()
+const emptyImage = new Image();
 
 export default class Loader {
-  constructor () {
-    this.loaded = 0
-    this.total = 0
-    this.loading = false
+  constructor() {
+    this.loaded = 0;
+    this.total = 0;
+    this.loading = false;
 
-    this.$resource = Object.create(null)
-    this.$resource.maps = Object.create(null)
-    this.$resource.image = Object.create(null)
-    this.$resource.sprites = Object.create(null)
+    this.$resource = Object.create(null);
+    this.$resource.maps = Object.create(null);
+    this.$resource.image = Object.create(null);
+    this.$resource.sprites = Object.create(null);
   }
 
-  init (config) {
-    this.loading = true
-    config.init.map((item) => this.loadJSON(item))
+  init(config) {
+    // this.loading = true
+    // config.init.map((item) => this.loadJSON(item))
 
     // .then(
     //   (...all) => {
     //     config.init.forEach((item, index) => {
     //       config[item] = all[index];
     //     });
-    //     console.log(config)
-    //     // this.loadMapping();
-    //     // this.loadImage();
-    //     // this.loadSprite();
     //   }
     // )
+    this.config = config;
+    this.loadMapping();
+    this.loadImage();
+    this.loadSprite();
   }
 
-  checkStatus () {
+  checkStatus() {
     if (this.loaded === this.total) {
-      this.loading = false
+      this.loading = false;
     }
   }
 
-  loadMapping () {
-    this.config.mapping.forEach((name) => {
-      this.total++
+  loadMapping() {
+    this.config.resource.mapping.forEach((name) => {
+      this.total++;
       loadText(`Data/${name}`).then((data) => {
-        this.loaded++
-        this.$resource.mapping = data
-      })
-    })
+        this.loaded++;
+        this.$resource.mapping = data;
+      });
+    });
   }
 
-  loadImage () {
-    this.config.images.forEach((name) => {
-      this.total++
+  loadImage() {
+    this.config.resource.images.forEach((name) => {
+      this.total++;
       loadImage(`Image/${name}`).then((data) => {
-        this.$resource.image[name] = data
-        this.loaded++
-        this.checkStatus()
-      })
-    })
+        this.$resource.image[name] = data;
+        this.loaded++;
+        this.checkStatus();
+      });
+    });
   }
 
-  loadMovie () {
-    this.config.images.forEach((name) => {
-      this.total++
+  loadMovie() {
+    this.config.resource.images.forEach((name) => {
+      this.total++;
       loadMovie(`Image/${name}`).then((data) => {
-        this.$resource.image[name] = data
-        this.loaded++
-        this.checkStatus()
-      })
-    })
+        this.$resource.image[name] = data;
+        this.loaded++;
+        this.checkStatus();
+      });
+    });
   }
 
-  loadSprite () {
-    this.config.sprites.forEach((name) => {
-      this.total++
+  loadSprite() {
+    this.config.resource.sprites.forEach((name) => {
+      this.total++;
       loadImage(`Sprite/${name}.png`).then((data) => {
-        this.$resource.image[name] = data
-        this.loaded++
-        this.checkStatus()
-      })
-      this.total++
+        this.$resource.image[name] = data;
+        this.loaded++;
+        this.checkStatus();
+      });
+      this.total++;
       loadText(`Sprite/${name}.dat`).then((data) => {
-        this.$resource.sprites[name] = data
-        this.loaded++
-        this.checkStatus()
-      })
-    })
+        this.$resource.sprites[name] = data;
+        this.loaded++;
+        this.checkStatus();
+      });
+    });
   }
 
-  loadMap (id) {
+  loadMap(id) {
+    console.log("inin");
     if (this.$resource.maps[id]) {
-      return Promise.resolve(this.$resource.maps[id])
+      return Promise.resolve(this.$resource.maps[id]);
     }
-    this.loaded = 0
-    this.total = 0
+    this.loaded = 0;
+    this.total = 0;
 
-    this.total++
+    this.total++;
     return loadJSON(`Maps/${id}.json`).then((data) => {
-      this.$resource.maps[id] = data
-      this.loaded++
-      this.checkStatus()
-      return data
-    })
+      this.$resource.maps[id] = data;
+      this.loaded++;
+      this.checkStatus();
+      return data;
+    });
   }
 
-  loadJSON (id) {
-    this.loaded = 0
-    this.total = 0
+  loadJSON(id) {
+    this.loaded = 0;
+    this.total = 0;
 
-    this.total++
+    this.total++;
     return loadJSON(`Data/${id}.json`).then((data) => {
-      this.$resource.maps[id] = data
-      this.loaded++
-      this.checkStatus()
-      return data
-    })
+      this.$resource.maps[id] = data;
+      this.loaded++;
+      this.checkStatus();
+      return data;
+    });
   }
 
-  loadConfig () {
-    return loadJSON('config.json')
+  loadConfig() {
+    return loadJSON("config.json");
   }
 
-  loadImageNext (url) {
-    this.$resource.image[url] = emptyImage
-    this.total++
+  loadImageNext(url) {
+    this.$resource.image[url] = emptyImage;
+    this.total++;
     if (!url) {
-      return
+      return;
     }
-    return loadImage(`Image/${url}`).then((data) => {
-      this.$resource.image[url] = data
-      this.loaded++
-      this.checkStatus()
-    }).catch(e => {
-
-    })
+    return loadImage(`Image/${url}`)
+      .then((data) => {
+        this.$resource.image[url] = data;
+        this.loaded++;
+        this.checkStatus();
+      })
+      .catch((e) => {});
   }
 }
--- src/Engine/index.js
+++ src/Engine/index.js
@@ -1,44 +1,44 @@
-import Render from './core/Render'
-import Loader from './core/Loader'
-import Sound from './core/Sound'
-import EventEmitter from './core/EventEmitter'
+import Render from "./core/Render";
+import Loader from "./core/Loader";
+import Sound from "./core/Sound";
+import EventEmitter from "./core/EventEmitter";
 
-import { createNode, patchNode } from './core/Node'
+import { createNode, patchNode } from "./core/Node";
 
 export default class Engine {
-  constructor ($app) {
-    this.$app = $app
+  constructor($app) {
+    this.$app = $app;
     if (this.checkRunTime()) {
-      this.$loader = new Loader()
+      this.$loader = new Loader();
       this.$loader
         .loadConfig()
-        .catch((error) => alert('config.json不存在' + error))
-        .then((config) => this.init(config))
+        .catch((error) => alert("config.json不存在" + error))
+        .then((config) => this.init(config));
     } else {
-      alert('不能直接运行index.html 或者 Chrome版本太老')
+      alert("不能直接运行index.html 或者 Chrome版本太老");
     }
   }
 
-  checkRunTime () {
-    if (location.protocol === 'file:' || !navigator.userAgentData) {
-      return false
+  checkRunTime() {
+    if (location.protocol === "file:" || !navigator.userAgentData) {
+      return false;
     } else {
-      return true
+      return true;
     }
   }
 
-  init (config) {
-    document.title = config.title
+  init(config) {
+    document.title = config.title;
 
-    this.$config = config // 游戏配置，不允许更改
+    this.$config = config; // 游戏配置，不允许更改
 
-    this.$state = { save: Object.create(null) } // 游戏存档，可以修改
+    this.$state = { save: Object.create(null) }; // 游戏存档，可以修改
 
-    this.$loader.init(config) // 资源加载器
+    this.$loader.init(config); // 资源加载器
 
-    this.$sound = new Sound(config, this.$loader) // 音频控制器
+    this.$sound = new Sound(config, this.$loader); // 音频控制器
 
-    this.$render = new Render(config, this.$loader) // 渲染器 + 事件控制器
+    this.$render = new Render(config, this.$loader); // 渲染器 + 事件控制器
 
     this.$event = new EventEmitter({
       $config: this.$config,
@@ -46,28 +46,28 @@ export default class Engine {
       $loader: this.$loader,
       $sound: this.$sound,
       $render: this.$render,
-    })
+    });
 
-    this.$root = null
-    this.gameStart()
+    this.$root = null;
+    this.gameStart();
   }
 
-  gameStop () {
-    cancelAnimationFrame(this.$requestAnimationId)
+  gameStop() {
+    cancelAnimationFrame(this.$requestAnimationId);
   }
 
-  gameStart () {
+  gameStart() {
     const next = () => {
       this.$requestAnimationId = requestAnimationFrame(() => {
-        this.keyFrame()
-        next()
-      })
-    }
-    next()
+        this.keyFrame();
+        next();
+      });
+    };
+    next();
   }
 
-  keyFrame () {
-    this.$root = patchNode(this.$root, createNode.call(this, this.$app))
-    this.$render.render(this.$root) // 渲染开始
+  keyFrame() {
+    this.$root = patchNode(this.$root, createNode.call(this, this.$app));
+    this.$render.render(this.$root); // 渲染开始
   }
 }
